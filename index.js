fab376740a4f67bafa8c440e162e11d7548b627c
WhiteBitch1488 <nmir388@gmail.com>
2024-12-24 19:41:40 +1000

    up

--- index.js
+++ index.js
@@ -5,26 +5,25 @@ const body = document.querySelector("body");
 const btn1 = document.getElementById("4");
 const btn2 = document.getElementById("5");
 
-btn1.addEventListener('click', () => {
-   const name = prompt("Введите ваше имя");
-body.classList.add("pic");
-   str1.classList.add("hidden");
-   str1.classList.remove("show");
-   str2.classList.add("show");
-   str2.classList.remove("hidden");  
-   btn1.classList.add('hidden');
-   btn2.classList.remove("hidden");
-   btn2.classList.add("show");
+btn1.addEventListener("click", () => {
+  const name = prompt("Введите ваше имя");
+  body.classList.add("pic");
+  str1.classList.add("hidden");
+  str1.classList.remove("show");
+  str2.classList.add("show");
+  str2.classList.remove("hidden");
+  btn1.classList.add("hidden");
+  btn2.classList.remove("hidden");
+  btn2.classList.add("show");
 
-   str2.querySelector('span').innerText = name;
-})
+  str2.querySelector("span").innerText = name;
+});
 
-btn2.addEventListener('click', () => {
-   str2.classList.remove("show");
-   str2.classList.add("hidden");
-   str3.classList.remove("hidden");
-   str3.classList.add("show");
-   btn1.classList.add("hidden");
-   btn2.classList.add("hidden");
-
-})
+btn2.addEventListener("click", () => {
+  str2.classList.remove("show");
+  str2.classList.add("hidden");
+  str3.classList.remove("hidden");
+  str3.classList.add("show");
+  btn1.classList.add("hidden");
+  btn2.classList.add("hidden");
+});
