525719e4fa1ecf06ac5f881461c71d5cf7e18322
WhiteBitch1488 <nmir388@gmail.com>
2024-12-24 19:04:59 +1000

    up

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const str1 = document.getElementById("1");
 const str2 = document.getElementById("2");
+const str3 = document.getElementById("3");
 const body = document.querySelector("body");
-const btn = document.querySelector('button');
+const btn1 = document.getElementById("4");
+const btn2 = document.getElementById("5");
 
-btn.addEventListener('click', () => {
+btn1.addEventListener('click', () => {
    const name = prompt("Введите ваше имя");
 body.classList.add("pic");
    str1.classList.add("hidden");
    str1.classList.remove("show");
    str2.classList.add("show");
-   str2.classList.remove("hidden");     
-   btn.classList.add('hidden');
+   str2.classList.remove("hidden");  
+   btn1.classList.add('hidden');
+   btn2.classList.remove("hidden");
+   btn2.classList.add("show");
 
    str2.querySelector('span').innerText = name;
+
+})
+
+btn2.addEventListener('click', () => {
+   str2.classList.remove("show");
+   str2.classList.add("hidden");
+   str3.classList.remove("hidden");
+   str3.classList.add("show");
+   btn1.classList.add("hidden");
+   btn2.classList.add("hidden");
 
 })
